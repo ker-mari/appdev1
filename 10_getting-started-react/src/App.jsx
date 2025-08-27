8451bb7ec5febf8968529a1741a1e985b3ecbb04
ker-mari <magmayoker@gmail.com>
2025-08-27 12:22:54 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+
+    <div>
+      <h1>About Me</h1>
+
+      <ul>
+        <li>Name: Kermari Magmayo</li>
+        <li>Course/Year: BS Information Technology 3rd Year</li>
+        <li>Fun Fact: I can reach my nose with my tongue, and somehow I'm bit flexible.</li>
+      </ul>
+      <h2>Why do you want to learn React?</h2>
+      <p>
+        React is one of the components that we will be using to build our projects and activities this semester.
+      </p>
+    </div>
+  );
+}
+
+export default App
