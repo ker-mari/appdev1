3025462079163daf995c8e074de44d23fdea0d11
ker-mari <magmayoker@gmail.com>
2025-09-06 09:33:48 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,27 @@
+//Number two
+
+let name = "Kermari";
+let age = 20;
+let isStudent = true;
+
+// console.log("Name:" + name,"Age:" + age, "isStudent:" + isStudent) 
+
+//I used Template Literals 
+console.log(`Name: ${name} , Age: ${age}, isStudent: ${isStudent}`)
+
+console.log(typeof name, typeof age, typeof isStudent)
+
+let num1 = 20;
+let num2 = 2;
+
+console.log (num1 + num2);
+
+console.log (num1 - num2);
+
+console.log (num1 * num2);
+
+console.log (num1 / num2);
+
+
+console.log("5"==5);
+console.log("5"===5);
