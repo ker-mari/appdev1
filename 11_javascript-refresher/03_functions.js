8c07a9d820ca591b2b5f1882fc5c40e17ad6bb7c
ker-mari <magmayoker@gmail.com>
2025-09-06 09:34:26 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_functions.js
@@ -0,0 +1,20 @@
+//Numero Tres
+
+function greet(name){
+    return `Hello ${name} kumusta?`
+}
+
+console.log(greet("Kermari"));
+
+const square = (num) => num * num;
+
+console.log(square(7));
+
+function calculate(a,b) {
+    return {
+        sum: a + b,
+        product: a * b
+    };
+}
+
+console.log(calculate(10,29));
