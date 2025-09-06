ff92e5135d338c0ad411c456188097ae0fd41620
ker-mari <magmayoker@gmail.com>
2025-09-06 10:30:25 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,16 @@
+var name = "Ker"; 
+var age = 20;
+age = 25; // na-reassigned siya
+
+console.log("var:", name, age);
+
+let name2 = "Kermari";
+const age2 = 9;
+
+console.log(name2, age2);
+
+name2 = "Kermari2";
+// age2 = 18; kinoment kasi nageerror kapag hindi, dahil const ang age2
+
+console.log(name2, age2);
+
