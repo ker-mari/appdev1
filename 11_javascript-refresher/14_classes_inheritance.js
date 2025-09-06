3f484d7209d31435b66bf4a7e386dd984f1465b5
ker-mari <magmayoker@gmail.com>
2025-09-06 10:59:51 +0800

    Add classes and inheritance activity

---
+++ 11_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  
+  sayHello() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+
+const student = new Student("Mari");
+student.sayHello(); 
+student.study();   
