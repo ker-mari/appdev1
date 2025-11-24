5ef064616afd80e1976d69406cef7b10a0de3ecc
ker-mari <magmayoker@gmail.com>
2025-11-24 13:19:38 +0800

    Done with when and how to create multi-component files

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,8 +1,8 @@
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Gallery from './components/Gallery';
 
 function Profile() {
   return (
