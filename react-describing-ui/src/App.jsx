e58fd34a6910781229d5b34dcf6989aed808ee6c
ker-mari <magmayoker@gmail.com>
2025-11-24 13:47:03 +0800

    Done with how to configure components with props

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,30 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import Gallery from './components/Gallery';
 import ToDoList from './components/ToDoList';
-
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
-
+import Profile from './components/Profile';
 
 function App() {
 
   return (
     <>
+      {/* Done with how to write your first React component */}
       <section>
         <h1>Amazing scientists</h1>
-        <Profile/>
-        <Profile/>
-        <Profile/>
+        <Profile />
+        <Profile />
+        <Profile />
       </section>
 
-      <Gallery/>
-      <ToDoList/>
+      {/* Done with when and how to create multi-component files */}
+      <Gallery />
+
+      {/* Done with how to add markup to JavaScript with JSX */}
+      {/* Done with how to use curly braces with JSX to access JavaScript functionality from your components */}
+      <ToDoList />
+
+      {/* Done with how to configure components with props */}
+      <Profile />
 
     </>
   )
--- react-describing-ui/src/components/Profile.jsx
+++ react-describing-ui/src/components/Profile.jsx
@@ -1,8 +1,20 @@
+import Avatar from "./Avatar";
+import Card from "./Card";
+
+
+src = "https://i.imgur.com/QIrZWGIs.jpg"
+alt = "Alan L. Hart"
+
 export default function Profile() {
-    return (
-        <img
-            src="https://i.imgur.com/QIrZWGIs.jpg"
-            alt="Alan L. Hart"
+  return (
+    <Card>
+        <Avatar
+            size={100}
+            person={{
+                name: 'Katsuko Saruhashi',
+                imageId: 'YfeOqp2'
+            }}
         />
-    );
+    </Card>
+  );
 }
