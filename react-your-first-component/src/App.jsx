946e0c398920f92d257c3912ead79c288862270c
ker-mari <magmayoker@gmail.com>
2025-11-24 14:11:39 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './components/Profile.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Profile />
+    </>
+  )
+}
+
+export default App
